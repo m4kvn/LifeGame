ad4e8a7d682dd34efcb1d09de506b716a5afccf5
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-01 23:00:29 +0900

    依存関係にProcessingを追加

---
+++ src/main/kotlin/com/masahirosaito/fun/lifegame/LifeGame.kt
@@ -0,0 +1,13 @@
+package com.masahirosaito.`fun`.lifegame
+
+import processing.core.PApplet
+
+class LifeGame : PApplet() {
+
+    override fun setup() {
+        size(500, 500)
+    }
+
+    override fun draw() {
+    }
+}
---
+++ src/main/kotlin/com/masahirosaito/fun/lifegame/Main.kt
@@ -0,0 +1,7 @@
+package com.masahirosaito.`fun`.lifegame
+
+import processing.core.PApplet
+
+fun main(args: Array<String>) {
+    PApplet.main("com.masahirosaito.fun.lifegame.LifeGame")
+}
